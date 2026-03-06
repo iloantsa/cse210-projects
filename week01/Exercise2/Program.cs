81fd3678f25b3b5e5f1035036dd0b108a9d6ff5f
iloantsa <ton@email.com>
2026-03-06 16:10:53 +0100

    FINISHED EX 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,78 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade: ");
+        string grade = Console.ReadLine();
+        int x = int.Parse(grade);
+        string letter = "";
+        // Get the last digit
+        float lastDigit = x % 10;
+        string sign = "";
+
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (x >= 60)
+        {
+            letter = "D";
+
+
+        }
+
+        else
+        {
+            letter = "F";
+        }
+       
+        //determine the sign
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit< 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        // Handle exception(A+, F+, F-)
+        if (letter == "A" && sign == "+" || letter == "F" )
+        {
+            sign = "";
+
+
+        }
+
+
+        Console.WriteLine($"You have earned the grade {letter}{sign}");
+        if (x >= 70)
+        {
+            Console.WriteLine($"Congratulation!, You passed the course.");
+        }
+        else 
+
+        {
+            Console.WriteLine($"Sorry! Please try the course again.");
+        }
+
     }
 }
