76e3ed42eea411b01845d60d062cd30cbc8e002e
iloantsa <ton@email.com>
2026-03-09 07:00:20 +0100

    EXERCICE 5 finished

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Net.NetworkInformation;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int Square = SquareNumber(userNumber);
+        DisplayResult(userName, Square);
+
     }
+    static void DisplayWelcome()
+
+    {
+        Console.WriteLine("Welcome to the Program!");
+            
+    }
+
+
+    static string PromptUserName()
+    {
+        Console.Write($"Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write($"Please enter your favorite: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+
+
+    }
+    static void DisplayResult(string userName, int square)
+    {
+        Console.WriteLine($"{userName}, the square of you number is: {square}");
+
+    }   
 }
+
+
+
+
