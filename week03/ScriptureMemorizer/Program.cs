c8e82ece81b83434aa43842b000039495bc95ac3
iloantsa <ton@email.com>
2026-04-04 23:43:09 +0200

    week 3 assignment

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,34 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+
+        string text = "Trust in the Lord with all thine heart and lean not unto thine own understanding";
+
+        Scripture scripture = new Scripture(reference, text);
+
+        while (!scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+
+            Console.WriteLine(scripture.GetDisplayText());
+
+            Console.WriteLine("Press enter key to continue or type the word'quit': ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(2);
+        }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
     }
 }
